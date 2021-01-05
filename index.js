1e58ba43eaa1ec0474e0f4eb41a64c3ea290d30c
Michael Iodice <miodice3@gmail.com>
2021-01-05 13:51:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let a = document.getElementById("main")
+a.remove()
+
+let newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
